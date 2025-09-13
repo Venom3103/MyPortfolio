357de09e44382a5aaadadf1e4dac9f296ce798c3
Ayush <Ayushkumar3300@gmail.com>
2025-09-13 16:24:54 +0530

    Update Contact.tsx

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -3,7 +3,7 @@ export default function Contact(){
     <section id="contact" className="glass p-8 rounded-2xl text-center space-y-4">
       <h3 className="text-xl font-semibold text-center">Get in touch</h3> <br></br>
   <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4 mt-4 justify-items-center">
-    <span>    <a href="https://drive.google.com/file/d/1XW9X8qe-QQSgFWd7Ts6BdpxLbwgf44aV/view?usp=sharing" target="_blank" rel="noreferrer" className="px-4 py-2 rounded-md glass">Resume</a></span>
+    <span>    <a href="https://drive.google.com/file/d/169h7Mq7v0a0R7LAbtkfb-Hb1VYzEHDnL/view?usp=sharing" target="_blank" rel="noreferrer" className="px-4 py-2 rounded-md glass">Resume</a></span>
     <span>    <a href="mailto:ayushkumar3300@gmail.com" target="_blank" rel="noreferrer" className="px-4 py-2 rounded-md glass">Gmail</a></span>
     <span>    <a href="https://github.com/Venom3103" target="_blank" rel="noreferrer" className="px-4 py-2 rounded-md glass">GitHub</a></span>
       <span> <a href="https://www.linkedin.com/in/ayush-k-2976b32a7" target="_blank" rel="noreferrer" className="px-4 py-2 rounded-md glass">LinkedIn</a></span>
